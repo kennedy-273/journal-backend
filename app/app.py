728174e1faecd152923e3255eeec660e02f440ba
Kennedy Bernard <makuanikennedy@gmail.com>
2024-07-09 15:06:45 +0300

    fixed  few bugs of fetching,data

--- app/app.py
+++ app/app.py
@@ -330,5 +330,4 @@ class JournalsByDate(Resource):
 api.add_resource(JournalsByDate, '/journals/<string:time_frame>')
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
+    app.run(debug=True, port=5500)
